72fd092e89f20ffd1a1af3c93f37a8db07d715c4
Ckala62rus <agr.akyla@mail.ru>
2025-01-26 19:17:50 +0300

    ( feature ) added user fsm state for create.

---
+++ src/fsm/user_fsm.py
@@ -0,0 +1,11 @@
+from aiogram.fsm.state import StatesGroup, State
+
+
+class CreateNewIssue(StatesGroup):
+    description = State()
+
+    texts = {
+        'AddProduct:description': 'Введите описание обращения:',
+        'AddProduct:price': 'Введите стоимость заново:',
+        'AddProduct:image': 'Этот стейт последний, поэтому...',
+    }

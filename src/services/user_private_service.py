659ad5e0a768d9b347f0de45ab55cab0174a3d59
Ckala <agr.akyla@mail.ru>
2025-03-12 08:26:42 +0300

    ( refactor ) moved scss paginate logic to user service.

--- src/services/user_private_service.py
+++ src/services/user_private_service.py
@@ -1,11 +1,12 @@
 import logging
 
 from aiogram import types
-from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup, Message
 
+from api.itilium_api import ItiliumBaseApi
 from bot_enums.user_enums import UserText, UserButtonText
+from dto.paginate_scs_dto import PaginateScsDTO
 from kbds.reply import get_keyboard
-
 
 logger = logging.getLogger(__name__)
 
@@ -29,3 +30,32 @@ async def base_start_handler(message: types.Message) -> None:
             str(UserButtonText.MENU)
         ])
     )
+
+
+async def paginate_scs_logic(
+    callback: types.CallbackQuery,
+    paginate_dto: PaginateScsDTO,
+) -> dict:
+    user = await ItiliumBaseApi.get_employee_data_by_identifier(callback)
+
+    if user is None:
+        return {}
+
+    logger.debug(f"user: {user['servicecalls']}")
+
+    await callback.answer()
+    send_message_for_search = await callback.message.answer("Запрашиваю заявки, подождите...")
+
+    my_scs: list = user['servicecalls']
+
+    if not my_scs:
+        await callback.answer()
+        await send_message_for_search.delete()
+        await callback.message.answer("У вас нет созданных заявок заявок")
+        return {}
+
+    results = await ItiliumBaseApi.get_task_for_async_find_sc_by_id(scs=my_scs, callback=callback)
+
+    paginate_dto.set_cache_scs(results)
+
+    return {"send_message_for_search": send_message_for_search}

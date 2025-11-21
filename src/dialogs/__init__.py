6b484765adff009b60b75eb76b3d846753f39291
Ckala <agr.akyla@mail.ru>
2025-11-21 10:38:39 +0300

    ( feature ) added states for auth registration.

--- src/dialogs/__init__.py
+++ src/dialogs/__init__.py
@@ -1,12 +1,15 @@
 from aiogram import Dispatcher
 from aiogram_dialog import setup_dialogs
 
-from . import bot_menu
+from . import bot_menu, registration
 
 
 def custom_setup_dialogs(dp: Dispatcher):
 
     for dialog in bot_menu.bot_menu_dialogs():
+        dp.include_router(dialog)
+    
+    for dialog in registration.registration_dialogs():
         dp.include_router(dialog)
 
     setup_dialogs(dp)
